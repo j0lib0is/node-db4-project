86366102e0cc7a176cac5152d2b0399aefa74c0a
jacobjolibois <jacobjolibois@hey.com>
2022-02-17 18:22:19 -0600

    finished formatting the returned recipe

--- api/recipes-model.js
+++ api/recipes-model.js
@@ -5,59 +5,44 @@ async function getRecipeById(recipe_id) {
 	const result = await db('recipes')
 		.join('steps', 'recipes.recipe_id', 'steps.recipe_id')
 		.join('step_ing as si', 'si.step_id', 'steps.step_id')
+		.join('ingredients', 'ingredients.ingredient_id', 'si.ingredient_id')
 		.where('recipes.recipe_id', recipe_id)
-		.select('recipes.recipe_id', 'recipe_name', 'steps.step_id', 'step_number', 'step_instructions');
+		.select('recipes.recipe_id', 'recipe_name', 'steps.step_id', 'step_number', 'step_instructions', 'ingredients.ingredient_id', 'ingredient_name', 'quantity');
 
 	if (result[0].length == 0) {
 		return null;
 	}
 
-	const recipe = {
+	const constructedRecipe = {
 		recipe_id: result[0].recipe_id,
 		recipe_name: result[0].recipe_name,
 		steps: []
 	};
 
 	if (result[0].step_id == null) {
-		return recipe;
+		return constructedRecipe;
 	}
 
 	for (let step of result) {
-		recipe.steps.push({
+		const constructedStep = {
 			step_id: step.step_id,
 			step_number: step.step_number,
 			step_instructions: step.step_instructions,
-		});
+			ingredients: []
+		}
+
+		for (let step of result) {
+			constructedStep.ingredients.push({
+				ingredient_id: step.ingredient_id,
+				ingredient_name: step.ingredient_name,
+				quantity: step.quantity
+			});
+		}
+		
+		constructedRecipe.steps.push(constructedStep);
 	}
 
-	console.log(recipe);
-	return recipe;
-
-	// - Should resolve a representation of the recipe similar to the one shown in the **Data Model** above.
-  // - The function will pull information from several tables using Knex and then create a response object using loops, objects, array methods etc.
-  // - There are many ways to solve this, but from a performance standpoint the fewer trips to the database the better!
-
-	// {
-	// 	"recipe_id" : 1,
-	// 	"recipe_name": "Spaghetti Bolognese",
-	// 	"created_at": "2021-01-01 08:23:19.120",
-	// 	"steps": [
-	// 		{
-	// 			"step_id": 11,
-	// 			"step_number": 1,
-	// 			"step_instructions": "Put a large saucepan on a medium heat",
-	// 			"ingredients": []
-	// 		},
-	// 		{
-	// 			"step_id": 12,
-	// 			"step_number": 2,
-	// 			"step_instructions": "Add 1 tbsp olive oil",
-	// 			"ingredients": [
-	// 				{ "ingredient_id": 27, "ingredient_name": "olive oil", "quantity": 0.014 }
-	// 			]
-	// 		},
-	// 	]
-	// }
+	return constructedRecipe;
 }
 
 // Exports
